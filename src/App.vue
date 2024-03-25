00889bcba6b4750b403a9c2d2314a42951b9d76a
Shuiling <1668589925@qq.com>
2024-03-25 12:09:38 -0700

    吖

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,123 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <header>
+      <nav class="navbar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section class="carousel">
+        <!-- 这里可以使用第三方库如 Vue-Carousel-3D 或者任何其他轮播图插件 -->
+        <div class="carousel-item active">
+          <img src="https://via.placeholder.com/800x400" alt="Slide 1">
+          <p>Caption for slide 1</p>
+        </div>
+        <div class="carousel-item">
+          <img src="https://via.placeholder.com/800x400" alt="Slide 2">
+          <p>Caption for slide 2</p>
+        </div>
+        <!-- 更多的轮播项... -->
+      </section>
+      <section class="features">
+        <div class="feature">
+          <h2>Feature 1</h2>
+          <p>Description of feature 1.</p>
+        </div>
+        <div class="feature">
+          <h2>Feature 2</h2>
+          <p>Description of feature 2.</p>
+        </div>
+        <!-- 更多的特色项... -->
+      </section>
+    </main>
+    <footer>
+      <p>&copy; 2024 My Vue App. All rights reserved.</p>
+    </footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+#app {
+  text-align: center;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.navbar {
+  background-color: #333;
+  overflow: hidden;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.navbar ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.navbar ul li {
+  float: left;
+}
+
+.navbar ul li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.navbar ul li a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.carousel {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+}
+
+.carousel-item {
+  position: absolute;
+  width: 100%;
+  display: none;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+.carousel-item.active {
+  display: block;
+}
+
+@keyframes fade {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+
+.features {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+}
+
+.feature {
+  flex-basis: 30%;
+  margin: 10px;
+  text-align: center;
+}
+
+footer {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 }
 </style>
