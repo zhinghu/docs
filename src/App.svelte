685524d538e7acf5e67f2f68f181f2b79362e17d
myChinesePYL <3223285598@qq.com>
2024-03-30 11:21:23 +0800

    添加了导航栏

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,34 @@
+<script lang="ts">
+  import TopNavigator from "./component/navigator.svelte";
+</script>
+
 <main>
-  <h1>Hello world!</h1>
+  <TopNavigator />
+  <div id="main"></div>
+  <nav id="bottom-navigator">In 2024/3/30. By Github myChinesePYL</nav>
 </main>
+
+<style>
+  #bottom-navigator {
+    background-color: #333;
+    position: fixed;
+    left: 0px;
+    top: calc(100vh - 30px);
+    width: 100vw;
+    height: 30px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+  }
+
+  #main {
+    display: flex;
+    min-height: max-content;
+    align-items: center;
+    justify-content: flex-start;
+
+    background-color: #aaa;
+  }
+</style>
---
+++ src/component/navigator.svelte
@@ -0,0 +1,36 @@
+<nav id="navigator">
+  <ul>
+    <li>home</li>
+    <li>docs</li>
+    <li>examples</li>
+  </ul>
+</nav>
+
+<style>
+  #navigator {
+    background-color: #333;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100vw;
+    height: 50px;
+  }
+
+  ul {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    color: white;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  ul > li {
+    display: flex;
+    list-style: none;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
