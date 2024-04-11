75fbb1c454d89e79daf0763f8c8725994c9dce60
myChinesePYL <3223285598@qq.com>
2024-04-11 11:48:53 +0800

    做了home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Panel from "../components/panel.svelte";
+</script>
+
+<Panel />
+<main>
+  <h1>欢迎来到3个人做的杂乱的文档+示例(语文不好, 见谅)</h1>
+  <h4>by Github --> <span style="color: #0ff">myChinese(主)</span></h4>
+</main>
+
+<style>
+  main{
+    display: flex;
+    min-height: calc(100vh - 30px);
+
+    flex-direction: column;
+    align-items: center;
+    padding: 40px 20px 0px 20px;
+
+    font-family: Helvetica, monospace, Arial;
+  }
+</style>
