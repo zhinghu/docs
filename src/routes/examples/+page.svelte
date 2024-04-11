f97b8823cb0d5f161b63876a97fc669a6593cc98
myChinesePYL <3223285598@qq.com>
2024-04-11 12:14:02 +0800

    在Home人员列表中添加了跳转链接

--- src/routes/examples/+page.svelte
+++ src/routes/examples/+page.svelte
@@ -3,28 +3,3 @@
 </script>
 
 <Panel />
-<div id="test">
-  {#each (() => {
-    let rt = [];
-    rt.length = 25;
-    rt.fill("");
-
-    return rt;
-  })() as i}
-    Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Excepturi
-    blanditiis nemo reprehenderit pariatur dolorem! Error ducimus expedita
-    harum, quas, ullam non. Aliquam, blanditiis amet ex atque sequi, neque a
-    cum.<br />
-  {/each}
-</div>
-
-<style>
-  :global(*) {
-    margin: 0;
-    padding: 0;
-  }
-  :global(body) {
-    background-color: rgb(50, 50, 50);
-    color: #fff;
-  }
-</style>
